edbe91a21b8ffa2e9c67f28c7494348e0c886c05
Daniel Alamilla <daniel.alamilla.co@gmail.com>
2022-03-11 17:03:02 -0600

    add component testing configuration

---
+++ dollars-to-cents/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "transform": {
+        "^.+\\.js$": "babel-jest",
+        "^.+\\.svelte$": "svelte-jester"
+    },
+    "moduleFileExtensions": [
+        "js",
+        "svelte"
+    ],
+    "testEnvironment": "jsdom"
+}
